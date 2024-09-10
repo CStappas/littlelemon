fe6cecc3ef71ea1d3731a21266c6a0006cb80949
bigflopz <chris.stappas@gmail.com>
2024-09-09 20:31:42 -0400

    fixing pusblished website

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 import food1 from "../images/restauranfood.jpg";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
   const navigate = useNavigate();
-  
+
   const goToAboutPage = () => {
-    navigate('/booking');
+    navigate("/booking");
   };
   return (
     <section className="section1">
-        <div className="info">
-          <h2>Little Lemon</h2>
-          <h3>Chicago</h3>
-          <p>
-            We are a family owned Mediterranean restaurant, focused on
-            traditional recipes servred with a modern twist.
-          </p>
-          <button className="button-reserve" onClick={goToAboutPage}>Reserve Table</button>
-        </div>
-        <div className="image-container">
-          <img className="main-img" src={food1} alt="food" />
-        </div>
-      </section>
-  )
+      <div className="info">
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes servred with a modern twist.
+        </p>
+        <button className="button-reserve" onClick={goToAboutPage}>
+          Reserve Table
+        </button>
+      </div>
+      <div className="image-container">
+        <img className="main-img" src={food1} alt="food" />
+      </div>
+    </section>
+  );
 }
